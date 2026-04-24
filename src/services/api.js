fb5142ae39abfe9e1b1da5ed674aaddb7d332c31
Herik-Thiury <herikthiury2017@gmail.com>
2026-04-23 21:39:29 -0300

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,18 +4,4 @@ const api = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
-// Interceptor para injetar o token em todas as requisições
-api.interceptors.request.use(async (config) => {
-  const token = localStorage.getItem('@ConectaPro:token');
-  
-  if (token) {
-    // O padrão Bearer é o esperado pelo Spring Security que o time de back configurou
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
-
 export default api;
