952b996ae70a1e950fecf287a76813a05f682cb0
Alana Silva <alana.vit.ms@gmail.com>
2026-08-12 10:38:15 -0300

    fix: ajustando codigo antes do deploy no render

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-
+ 
 const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080'
 });
-
+ 
 api.interceptors.request.use((config) => {
   const userStorage = localStorage.getItem('@ConectaPro:user');
   
@@ -18,5 +18,5 @@ api.interceptors.request.use((config) => {
 }, (error) => {
   return Promise.reject(error);
 });
-
+ 
 export default api;
